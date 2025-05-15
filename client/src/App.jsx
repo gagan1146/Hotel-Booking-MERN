709f0eca77201ab5d496d12338f813f492430905
gagan1146 <gagansharma99690@gmail.com>
2025-05-15 15:29:20 +0530

    Updated Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar';
-import { useLocation } from 'react-router-dom';
+import Home from './Pages/Home';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
+  // const home = useLocation().pathname.includes("home");
   return (
     <div>
       {!isOwnerPath ?<Navbar/>:<></>}
+      {/* {home ?<Home/>:<></>} */}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
